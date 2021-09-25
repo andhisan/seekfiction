076d3508adf8282cb5556bb1f5a3c446276dfd7f
sasigume <sasigume@gmail.com>
2021-09-25 14:20:31 +0900

    fix(lint): :rotating_light: fixed lint error

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,4 +1,4 @@
-const admin = require('firebase-admin');
+import * as admin from 'firebase-admin';
 
 if (admin.apps.length === 0) {
   admin.initializeApp();
--- packages/api/src/lib/withAuth.ts
+++ packages/api/src/lib/withAuth.ts
@@ -6,11 +6,11 @@ const adminConfig = functions.config() as AdminConfig;
  * Authenticate request with key configured with functions:config:set
  *
  * @param {functions.https.Request} request
- * @param {functions.https.Response} response
+ * @param {functions.Response} response
  * @param {Function} next
  * @return {*}
  */
-const withAuth = (request: functions.https.Request, response: any, next: Function) => {
+const withAuth = (request: functions.https.Request, response: functions.Response, next: () => any): any => {
   const secret = request.headers.authorization as string | undefined;
 
   if (secret !== adminConfig.web.auth) {
