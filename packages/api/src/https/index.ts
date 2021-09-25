0e668c8556ba53db8c31e0504df1822a3b7de12a
sasigume <sasigume@gmail.com>
2021-09-25 14:09:43 +0900

    feat(firebase): :sparkles: added status check function

--- packages/api/src/https/index.ts
+++ packages/api/src/https/index.ts
@@ -1,1 +1,20 @@
+import * as functions from 'firebase-functions';
+import withAuth from '../lib/withAuth';
+
+// include functions under search directory
 export * as search from './search';
+
+/**
+ * Return API Status
+ *
+ * @param {functions.https.Request} request
+ * @param {functions.https.Response} response
+ * @return {void}
+ */
+exports.status = functions.region('us-central1').https.onRequest(async (request, response) => {
+  return withAuth(request, response, async () => {
+    return response.status(200).json({
+      message: `All OK`,
+    });
+  });
+});
