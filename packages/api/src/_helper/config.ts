f60091014dcb05c621363b33bcbbe2339250b261
sasigume <sasigume@gmail.com>
2021-09-25 17:42:34 +0900

    fix(firebase): :ambulance: fixed algolia index depends on environment

--- packages/api/src/_helper/config.ts
+++ packages/api/src/_helper/config.ts
@@ -1,2 +1,9 @@
 export const ANIME_COLLECTION = 'algolia_anime';
-export const ANIME_INDEX = 'test_anime';
+export const getAnimeIndex = (): string => {
+  // https://stackoverflow.com/a/63127747
+  if (process.env.FUNCTIONS_EMULATOR === 'true') {
+    return 'anime_dev';
+  } else {
+    return 'anime';
+  }
+};
--- packages/api/src/firestore/onWrite/algolia.ts
+++ packages/api/src/firestore/onWrite/algolia.ts
@@ -2,10 +2,10 @@ import * as functions from 'firebase-functions';
 import algolia from '../../_helper/algolia';
 import { syncAlgoliaWithFirestore } from '../../_helper/firebase-algolia-sync';
 
-import { ANIME_COLLECTION, ANIME_INDEX } from '../../_helper/config';
+import { ANIME_COLLECTION, getAnimeIndex } from '../../_helper/config';
 
 const syncTestAnime = functions.firestore.document(`/${ANIME_COLLECTION}/{id}`).onWrite((change, context) => {
-  const index = algolia.initIndex(ANIME_INDEX);
+  const index = algolia.initIndex(getAnimeIndex());
   return syncAlgoliaWithFirestore(index, change, context);
 });
 
