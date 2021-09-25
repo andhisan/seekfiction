007b4dd180638ae7ae8be9297be4d6f2387fe92b
sasigume <sasigume@gmail.com>
2021-09-25 22:06:24 +0900

    fix: :ambulance: fixed types not included in dist

--- packages/seekfiction-commons/index.ts
+++ packages/seekfiction-commons/index.ts
@@ -1,7 +1,21 @@
-import { AnimeOnAlgolia } from './types';
+interface AnimeForFirestore {
+  mal_id?: number | null;
+  aniList_id?: number | null;
+  kitsu_id?: number | null;
+  simkl_id?: number | null;
+  title_romaji: string | null;
+  mal_image?: string | null;
+  aniList_image?: string | null;
+  kitsu_image?: string | null;
+  simkl_image?: string | null;
+}
+
+interface AnimeOnAlgolia extends AnimeForFirestore {
+  objectID: string;
+}
 
 export const checkAnimeHasImage = (anime: AnimeOnAlgolia) => {
   return anime.mal_image !== undefined || anime.aniList_image !== undefined || anime.kitsu_image !== undefined || anime.simkl_image !== undefined;
 };
 
-export { AnimeOnAlgolia };
+export { AnimeForFirestore, AnimeOnAlgolia };
