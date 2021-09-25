c35539759b4418ad28050f432c162e1d844633bc
sasigume <sasigume@gmail.com>
2021-09-26 07:52:48 +0900

    fix(components): :lipstick: improved version display

--- packages/web-next/src/components/atoms/Version.tsx
+++ packages/web-next/src/components/atoms/Version.tsx
@@ -1,4 +1,4 @@
 export default function Version() {
   const v = require('../../../package.json').version;
-  return <div className="fixed top-3 left-3 opacity-50 font-mono">{v.toString()}</div>;
+  return <div className="fixed top-3 left-3 z-50 opacity-50 font-mono">client: v{v.toString()}</div>;
 }
