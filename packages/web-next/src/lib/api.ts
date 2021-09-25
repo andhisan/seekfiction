762c21c29ecca0105a57b195f2c63aca16750168
sasigume <sasigume@gmail.com>
2021-09-26 07:35:07 +0900

    fix(components): :bug: fixed wrong kitsu anime url

--- packages/web-next/src/lib/api.ts
+++ packages/web-next/src/lib/api.ts
@@ -3,7 +3,7 @@ import { AnimeOnAlgolia, ApiType } from '@sasigume/seekfiction-commons';
 export const urlConverter = (type: ApiType, id: AnimeOnAlgolia['mal_id'], slug: AnimeOnAlgolia['slug']) => {
   if (type == 'mal') return `https://myanimelist.net/anime/${id}`;
   if (type == 'aniList') return `https://anilist.co/anime/${id}`;
-  if (type == 'kitsu') return `https://kitsu.io/explore/anime/${slug}`;
+  if (type == 'kitsu') return `https://kitsu.io/anime/${slug}`;
   if (type == 'simkl') return `https://simkl.com/anime/${id}`;
   return `https://myanimelist.net/anime/${id}`;
 };
