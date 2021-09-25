9190e13ff9c6dff9a78ad4efc2cd34dda186f4fa
sasigume <sasigume@gmail.com>
2021-09-25 18:30:30 +0900

    refactor: :art: formatted algolia client code

--- packages/web-next/src/lib/algolia.ts
+++ packages/web-next/src/lib/algolia.ts
@@ -1,3 +1,4 @@
 import algoliasearch from 'algoliasearch/lite';
-
-export const algolia = algoliasearch(process.env.ALGOLIA_APP_ID ?? '', process.env.ALGOLIA_SEARCH_API_KEY ?? '');
+const appId = process.env.ALGOLIA_APP_ID ?? '';
+const apiKey = process.env.ALGOLIA_SEARCH_API_KEY ?? '';
+export const algolia = algoliasearch(appId, apiKey);
