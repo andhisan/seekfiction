375d38d42019a73440cb9f6ec542e2b479c1778f
sasigume <sasigume@gmail.com>
2021-09-27 02:53:40 +0900

    fix(test): :white_check_mark: fixed test fail because of recoil

--- packages/web-next/src/__tests__/pages/index.test.tsx
+++ packages/web-next/src/__tests__/pages/index.test.tsx
@@ -1,11 +1,16 @@
 import { shallow } from 'enzyme';
 import React from 'react';
+import { RecoilRoot } from 'recoil';
 
 import Home from '../../pages/index';
 
 describe('With Enzyme', () => {
   it('Home contains search form', () => {
-    const app = shallow(<Home />);
+    const app = shallow(
+      <RecoilRoot>
+        <Home />
+      </RecoilRoot>
+    );
     expect(app.find('form')).toBeTruthy();
   });
 });
