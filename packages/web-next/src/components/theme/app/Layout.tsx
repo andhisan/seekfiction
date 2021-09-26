d02ff49ee31e2f032a8a589b6ee7d606d5351445
sasigume <sasigume@gmail.com>
2021-09-27 03:10:35 +0900

    perf(og): :sparkles: added og image

--- packages/web-next/src/components/theme/app/Layout.tsx
+++ packages/web-next/src/components/theme/app/Layout.tsx
@@ -13,7 +13,11 @@ const Layout: React.FC = ({ children }) => {
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="A next-generation anime/manga database" />
+        <meta name="og:image" content="https://sf.sasigu.me/ogp-1200x630.png" />
+        <meta name="twitter:image" content="https://sf.sasigu.me/ogp-1200x630.png" />
+        <meta name="twitter:card" content="summary_largy_image" />
         <meta name="format-detection" content="telephone=no" />
+        <meta property="og:site_name" content="seekfiction" />
       </Head>
       <div className="flex flex-col min-h-screen">
         <main className="py-32 flex-grow flex flex-col gap-3 px-6 w-screen items-center">{children}</main>
