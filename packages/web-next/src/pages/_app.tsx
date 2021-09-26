c1b948507bd624674fb9a9e93383464f6822f7b3
sasigume <sasigume@gmail.com>
2021-09-26 23:48:48 +0900

    feat(components): :sparkles: installed recoil

--- packages/web-next/src/pages/_app.tsx
+++ packages/web-next/src/pages/_app.tsx
@@ -2,9 +2,15 @@ import '../styles/globals.css';
 import 'windi.css';
 import initApp from '@/lib/firebase';
 import type { AppProps } from 'next/app';
-
+import { RecoilRoot } from 'recoil';
+import { useNsfw } from '~/lib/nsfw-hook';
 function MyApp({ Component, pageProps }: AppProps) {
   initApp();
-  return <Component {...pageProps} />;
+
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
 export default MyApp;
