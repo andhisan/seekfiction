d93c06ce06bbbfc13be067e357e4ca4d05da6a11
sasigume <sasigume@gmail.com>
2021-09-27 04:24:55 +0900

    build: :fire: removed client version up

--- packages/web-next/src/__tests__/pages/index.test.tsx
+++ packages/web-next/src/__tests__/pages/index.test.tsx
@@ -6,6 +6,7 @@ import Home from '../../pages/index';
 
 describe('With Enzyme', () => {
   it('Home contains search form', () => {
+    // we need to mock index count
     const app = shallow(
       <RecoilRoot>
         <Home indexCount={0} />
