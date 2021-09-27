e215cdc736f158202f43eb87938a6b4b99fa0417
sasigume <sasigume@gmail.com>
2021-09-27 11:21:35 +0900

    fix(components): :lipstick: changed image display order

--- packages/web-next/src/components/molecules/algolia/HitComponent.tsx
+++ packages/web-next/src/components/molecules/algolia/HitComponent.tsx
@@ -10,12 +10,18 @@ interface Props {
   onClick: () => void;
 }
 
+/**
+ * Component to show one anime result
+ *
+ * @param {Props} props
+ * @return {*}
+ */
 export default function HitComponent(props: Props) {
   const { setAnimeId } = useAnimeId();
   const { open, setOpen } = useOpen();
   return (
     <>
-      {/* Open in new tab to keep search result */}
+      {/* Open detail window */}
       <a
         onClick={() => {
           setAnimeId(props.hit.objectID);
@@ -25,13 +31,15 @@ export default function HitComponent(props: Props) {
         title={props.hit.title_romaji ?? ''}
       >
         <div className={`relative ${props.hit.nsfw && 'nsfw'}`}>
-          <ImgBox type="mal" id={props.hit.mal_id} src={props.hit.mal_image} />
-          <ImgBox type="aniList" id={props.hit.aniList_id} src={props.hit.aniList_image} />
-          <ImgBox type="kitsu" id={props.hit.kitsu_id} src={props.hit.kitsu_image} />
+          {/* Put MAL to last because MAL has the most precise image */}
           <ImgBox type="simkl" id={props.hit.simkl_id} src={props.hit.simkl_image} />
+          <ImgBox type="kitsu" id={props.hit.kitsu_id} src={props.hit.kitsu_image} />
+          <ImgBox type="aniList" id={props.hit.aniList_id} src={props.hit.aniList_image} />
+          <ImgBox type="mal" id={props.hit.mal_id} src={props.hit.mal_image} />
         </div>
 
         <div style={{ background: props.hit.nsfw ? '#aa3344' : '#000' }} className="absolute z-10 text-lg bottom-0 bg-black text-white w-full p-3">
+          {/* Highlight search word in title */}
           <Highlight attribute="title_romaji" hit={props.hit} />
         </div>
       </a>
