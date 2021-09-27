2423288d6a758b3116eddbf901ef73a8a4e14a21
sasigume <sasigume@gmail.com>
2021-09-27 14:30:45 +0900

    fix(components): :ambulance: fixed react warning on image display

--- packages/web-next/src/components/atoms/ImgBox.tsx
+++ packages/web-next/src/components/atoms/ImgBox.tsx
@@ -8,8 +8,10 @@ import { ApiType } from '@sasigume/seekfiction-commons';
  * @param {*} props
  */
 const ImgBox: React.FC<{ type: ApiType; src?: string | null; id?: number | null; slug?: string | null }> = (props) => (
-  <div className="block absolute top-0 left-0">
-    <div className="w-[212px] h-[300px]">{props.src && <Image alt={`${props.type} ID:${props.id}`} src={props.src} layout="fill" />}</div>
+  <div className="block relative">
+    <div className="absolute top-0 left-0 w-[212px] h-[300px]">
+      {props.src && <Image alt={`${props.type} ID:${props.id}`} src={props.src} width="212px" height="300px" />}
+    </div>
   </div>
 );
 export default ImgBox;
