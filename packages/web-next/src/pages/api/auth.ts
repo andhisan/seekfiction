977a3d641c9cd7c7ba1fcfd7434349700765555f
sasigume <sasigume@gmail.com>
2021-09-28 02:05:05 +0900

    feat(vercel): :sparkles: created auth endpoint

---
+++ packages/web-next/src/pages/api/auth.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest as Req, NextApiResponse as Res } from 'next';
+
+export default async function auth(req: Req, res: Res) {
+  if (!req.headers.authorization || req.headers.authorization !== process.env.FUNCTIONS_AUTH) {
+    return res.status(403).json({ message: 'Unauthorized' });
+  }
+
+  const body = JSON.parse(req.body);
+  const path = body.path;
+
+  return await fetch(path, {
+    method: body.method ?? 'GET',
+    headers: {
+      Authorization: process.env.FUNCTIONS_AUTH ?? '',
+    },
+  })
+    .then(async (response) => {
+      const json = await response.json();
+      // Pass dataZZ as is
+      return res.status(response.status).send(json);
+    })
+    .catch((e) => {
+      return res.status(500).json({ message: 'Error occured while calling internal API: ' + JSON.stringify(e) });
+    });
+}
