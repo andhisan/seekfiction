8f869247cce5728dd2194cdddc6eb2d293832d4f
sasigume <sasigume@gmail.com>
2021-09-28 20:30:31 +0900

    refactor(firebase): :fire: removed algolia

--- packages/api/src/_helper/algolia.ts
+++
@@ -1,8 +0,0 @@
-import algoliasearch from 'algoliasearch';
-import * as functions from 'firebase-functions';
-import { AdminConfig } from '../models/AdminConfig';
-const adminConfig = functions.config() as AdminConfig;
-
-const algolia = algoliasearch(adminConfig.algolia.app_id, adminConfig.algolia.admin_api_key);
-
-export default algolia;
--- packages/api/src/models/AdminConfig.d.ts
+++ packages/api/src/models/AdminConfig.d.ts
@@ -2,11 +2,6 @@ export interface AdminConfig {
   web: {
     auth: string;
   };
-  algolia: {
-    app_id: string;
-    admin_api_key: string;
-    search_api_key: string;
-  };
   simkl: {
     client_id: string;
   };
